cb31132825295fa9b086778c2c0bd8edaefda499
Denis Peplin <denis.peplin@gmail.com>
2012-04-07 16:08:03 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MappedMonitor::Application.config.secret_token = '01b5ec9ee2dc2c129fa6a38af5f4488ebcfb7dc8a0fb9882282adc5daec57a4a3bc403d562e9880b76209f7c0bf4ea413896c853a0a3be09162c0cf80de12510'
