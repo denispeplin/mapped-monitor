123d06ead1203f7610264ac0abaee0c650d4f845
Denis Peplin <denis.peplin@gmail.com>
2012-04-08 17:18:12 +0400

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120407192159) do
+ActiveRecord::Schema.define(:version => 20120408131604) do
 
   create_table "points", :force => true do |t|
     t.float    "latitude"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120407192159) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "color"
   end
 
 end
